5ae9aa4d96db4bc4692e0da3c8a9b1f82c98f10a
OksanaVorobiova <ov172608@gmail.com>
2023-02-18 11:47:57 +0200

    responsive js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { save, load } from './js/locale-storage';
 import mobileMenu from './js/mobile-menu';
-
+import headerResponsive from './js/headerResponsive';
 
 
 mobileMenu();
+headerResponsive();
