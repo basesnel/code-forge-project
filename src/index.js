2c16b423bfbc166bed9e972b694e1c953c98969e
Ihor Bodianchuk <ihordobruy@gmail.com>
2023-02-19 19:36:28 +0200

    added base news

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@
 import { save, load } from './js/locale-storage';
 
 // HEADER SCRIPTS
-import "./js/open-search-form";
+import "./js/news-data/news-by-search";
 import './js/switcher-theme';
 import mobileMenu from './js/mobile-menu';
 import headerResponsive from './js/headerResponsive';
-import NewsApiService from './js/api/main-api'
-import "./js/category";
+import NewsApiService from './js/api/main-api';
+import "./js/news-data/news-category";
+
+// MAIN SCRIPTS
+import "./js/news-data/news-popular";
 
 mobileMenu();
 headerResponsive();
---
+++ src/js/news-data/news-category.js
@@ -0,0 +1,47 @@
+import NewsApiService from '../api/main-api'
+import Notiflix from 'notiflix';
+
+const categoryApiService = new NewsApiService();
+
+const refs = {
+    categories: document.querySelector('.js-category'),
+    categoriesOthers:document.querySelector('.js-category-others'),
+}
+getResponse();
+
+async function getResponse() {
+    try {
+        const response = await categoryApiService.getNewsCategoryList();
+        renderCategoriesDesctop(response.results);
+        renderCategoriesOtherDesctop(response.results);
+	} catch (error) {
+        Notiflix.Notify.failure('There are problems with your request.Please try again later.')
+    }
+    }
+
+function renderCategoriesDesctop(results) {
+    let markup = '';
+    for (let i = 0; i < 6; i++) {
+        markup += `
+            <li class="category__item"><button type="button">${results[i].display_name}</button></li>
+            `;
+    };
+    refs.categories.insertAdjacentHTML('beforeend', markup);
+    return 
+}
+
+function renderCategoriesOtherDesctop(results) {
+    let markup = '';
+    for (let i = 6; i < 50; i++) {
+        markup += `
+          <li class="category__others-list-item">
+            <button class="category__others-list-button">${results[i].display_name}</button>
+          </li>
+            `;
+    };
+    refs.categoriesOthers.insertAdjacentHTML('beforeend', markup);
+    return 
+}
+
+
+
