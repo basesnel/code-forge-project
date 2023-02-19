f71c4bd83ff8456ff972cf430e8b130cb6af085e
Maksym-Palii <paliym_@ukr.net>
2023-02-19 22:58:03 +0200

    record-date-in-input

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-
 import { save, load } from './js/locale-storage';
 
 // HEADER SCRIPTS
-import "./js/open-search-form";
+import './js/open-search-form';
 import './js/switcher-theme';
 import mobileMenu from './js/mobile-menu';
 import headerResponsive from './js/headerResponsive';
-import NewsApiService from './js/api/main-api'
-import "./js/category";
+import NewsApiService from './js/api/main-api';
+import './js/category';
+import './js/calendar';
 
 mobileMenu();
 headerResponsive();
-
-
